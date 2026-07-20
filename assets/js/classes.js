02f556a2303339d76e889647b9c6badb85cd8db6
Joao Martins <joaomartins102009@hotmail.com>
2026-07-20 16:39:56 -0300

    Criando Classes de Monstros

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -35,3 +35,23 @@ class Sorcerer extends Character{
         this.defense = 3;
     }
 }
+
+class LittleMonster extends Character{
+    constructor(){
+        super('Little Monster');
+        this.life = 40;
+        this.maxLife = this.life;
+        this.attack = 4;
+        this.defense = 4;
+    }
+}
+
+class BigMonster extends Character{
+    constructor(){
+        super('Big Monster');
+        this.life = 120;
+        this.maxLife = this.life;
+        this.attack = 16;
+        this.defense = 6;
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 let char = new Sorcerer('Jmz');
+let monster = new LittleMonster();
 
+console.log('Status do personagem:');
 console.log(char.name);
 console.log(char.life);
 console.log(char.attack);
+console.log('Status do monstro:');
+console.log(monster.name);
+console.log(monster.life);
+console.log(monster.attack);
